c122c87acce46c57198918dbcedfc9de39a3a407
atishay-sarvam <141825679+atishay-sarvam@users.noreply.github.com>
2023-12-02 06:26:05 +0000

    changes

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -1,23 +1,41 @@
 import streamlit as st
 from streamlit_chat import message
 import requests
+import time
 
 st.set_page_config(
     page_title="Streamlit Chat - Demo",
     page_icon=":robot:"
 )
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": st.secrets['api_key']}
+API_URL = "http://34.147.4.82:80/conversation"
 
-st.header("Streamlit Chat - Demo")
-st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+
+import streamlit as st
+
+access_code = st.text_input(
+    label="Enter API Key",
+    type="password",
+)
+
+while True:    
+    if access_code:
+        break
+    else:
+        print("sleepy")
+        time.sleep(1)
+headers = {"X-API-Key":access_code}
+
+
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
+if 'session_id' not in st.session_state:
+    st.session_state['session_id'] = ""
+
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -27,20 +45,18 @@ def get_text():
     input_text = st.text_input("You: ","Hello, how are you?", key="input")
     return input_text 
 
-
 user_input = get_text()
-
 if user_input:
     output = query({
-        "inputs": {
-            "past_user_inputs": st.session_state.past,
-            "generated_responses": st.session_state.generated,
+        "data": {
             "text": user_input,
-        },"parameters": {"repetition_penalty": 1.33},
+    },"session_id":st.session_state['session_id']
     })
 
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output["generated_text"])
+    st.session_state.session_id = output['session_id']
+    print(output)
+    st.session_state.generated.append(output["data"]['output'])
 
 if st.session_state['generated']:
 
